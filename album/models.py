a18d58a2860cf64cad57058c391b12dcdd76edf5
enockabere <maebaenock95@gmail>
2021-09-04 17:18:02 +0300

    create an image category model

--- album/models.py
+++ album/models.py
@@ -5,4 +5,8 @@ class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email=models.EmailField()
+class Category(models.Model):
+    name = models.CharField(max_length=30)
     
+    def __str__(self):
+        return self.name
