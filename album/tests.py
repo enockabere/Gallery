8fed29fc4f3de206052fd2dca09158f419216ada
enockabere <maebaenock95@gmail>
2021-09-04 19:27:01 +0300

    test case to save an image to db

--- album/tests.py
+++ album/tests.py
@@ -28,3 +28,15 @@ class ImageTest(TestCase):
         #creating new location and save it
         self.new_location = Location(places='Nairobi')
         self.new_location.save()
+        
+        self.new_image = Image(image_name="Personal", image_description="Nobody Sleeps in the Woods Tonight",user=self.enock)
+        self.new_image.save()
+        
+        self.new_image.category.add(self.new_category)
+        self.new_image.places.add(self.new_location)
+        
+    def tearDown(self):
+        User.objects.all().delete()
+        Image.objects.all().delete()
+        Category.objects.all().delete()
+        Location.objects.all().delete()
