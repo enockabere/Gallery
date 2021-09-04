6a1da13c36830370efaa8ed37181fd59bfa22388
enockabere <maebaenock95@gmail>
2021-09-04 21:37:29 +0300

    create a search url pattern

--- album/urls.py
+++ album/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.welcome,name='welcome'),
     path('all/', views.all_images,name='allImages'),
-    path('single/', views.single_image,name='singleImage')
+    path('single/', views.single_image,name='singleImage'),
+    path('search/', views.search_results,name='search_results')
 ]
