c3571e7ea6e21daddcc8e8cc5323d08e395135fd
enockabere <maebaenock95@gmail>
2021-09-06 00:38:58 +0300

    style hero section

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#overs").show(function(){
+      $("#overs").animate({
+        width: "100%",
+        opacity: 1,
+        left: "50px",
+        speed:"slow"
+      }, 4000);
+    });
+  });
